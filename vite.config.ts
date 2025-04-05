675d34f9849434c65f9f826e3873db2a2101bfd7
leejiun <leejiun@worksmobile.com>
2025-04-06 00:06:29 +0900

    fix: update base_url (vite.config)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import {VitePWA} from "vite-plugin-pwa";
 // import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
+    base: '/main',
+    plugins: [
       react(),
     // tailwindcss(),
       VitePWA({
