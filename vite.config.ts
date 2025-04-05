37a3cd77fd958c11d5a319cdb6f694f247a4d99f
leejiun <leejiun@worksmobile.com>
2025-04-05 22:54:04 +0900

    feat: add PWA configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
 // import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
       react(),
     // tailwindcss(),
+      VitePWA({
+          registerType: 'autoUpdate',
+          manifest: false, // manifest 직접 불러오게 설정
+          includeAssets: [
+              'favicon.svg',
+              'robots.txt',
+              'apple-touch-icon.png',
+              "pwa-192x192.png",
+              "pwa-512x512.png",
+          ],
+          srcDir: 'src',
+          filename: 'sw.js'
+      }),
 
   ],
     resolve: {
